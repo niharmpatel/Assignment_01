cbc78af7ba590f0ace8904a33756e203fcc6cc83
Nihar Patel <niharmpatel@gmail.com>
2019-10-15 22:13:59 -0400

    final commit

--- routes/index.js
+++ routes/index.js
@@ -4,24 +4,25 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* request home page and send it to server */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Nihar Patel' });
 });
 
-
+// request an about page and send it to server
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About me' });
 });
 
-
+// request an project page and send it server
 router.get('/project', function(req, res, next) {
   res.render('project', { title: 'Projects' });
 });
-
+// request an contact page and send it server
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact' });
 });
+// request an services page and send it server
 router.get('/services', function(req, res, next) {
   res.render('services', { title: 'Services Page' });
 });
